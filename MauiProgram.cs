acdff2c51eb4bb1e261f5480401ca1524aaa2346
Ali Hamza <alihamza.bcss19@iba-suk.edu.pk>
2024-04-19 20:41:57 +0500

    aa

---
+++ MauiProgram.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Maui;
+using DayOnes.Views;
+using Microsoft.Extensions.Logging;
+using Syncfusion.Maui.Core.Hosting;
+
+namespace DayOnes
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+            .UseMauiApp<App>()
+            // Initialize the .NET MAUI Community Toolkit by adding the below line of code
+            .UseMauiCommunityToolkit();
+            builder.Services.AddTransient<FSettingsPage>();
+
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, view) =>
+            {
+#if __ANDROID__                 
+                handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent); 
+#elif __IOS__                 
+                handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;                 
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None; 
+#endif
+            });
+
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("SF-Pro-Text-Regular.otf", "SFProText");
+
+                    fonts.AddFont("Brands-Regular-400.otf", "FAB");
+                    fonts.AddFont("Free-Regular-400.otf", "FAR");
+                    fonts.AddFont("Free-Solid-900.otf", "FAS");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
---
+++ Views/FanPages/RegFanPage.xaml.cs
@@ -0,0 +1,48 @@
+
+
+namespace DayOnes.Views;
+
+public partial class RegFanPage : ContentPage
+{
+
+    private bool _isHost;
+
+    public RegFanPage()
+	{
+		InitializeComponent();
+        Shell.SetBackButtonBehavior(this, new BackButtonBehavior
+        {
+            IsVisible = false
+        });
+    }
+
+   
+
+    private void OnToggled(object sender, ToggledEventArgs e)
+    {
+        //Invert the field on toggle
+        _isHost = !_isHost;
+    }
+
+    private async void btnLogin_Click(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync($"///{nameof(LoginPage)}");
+    }
+
+    private void btnRegister_Click(object sender, EventArgs e)
+    {
+        //fields
+        var fullName = this.txtFullName.Text;
+        var userName = this.txtUsername.Text;
+        var email = this.txtEmail.Text;
+        var phone = this.txtPhone.Text;
+        var password = this.txtPassword.Text;
+        var confirmPassword = this.txtConfirmPassword.Text;
+
+        Shell.Current.GoToAsync(nameof(FHomePage));
+
+        //if user is a client then call call AWS - API: RegC1 with all the above fields
+//        If the user is a Host, then they will press the Switch on this page where the
+//app will navigate to Page: Reg HOST
+    }
+}
